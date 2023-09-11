75364fc282493a14b57da42681ff7764326ec51e
ade <harounadelani@gmail.com>
2023-09-11 14:18:04 +0100

    header changed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <div className="header flex justify-between py-4 border-b">
             <article className="flex flex-col items-start">
-                <h1 className="text-2xl">Delanis Lottery</h1>
+                <h1 className="text-2xl">Lanis Lottery</h1>
                 <p className=" italic text-xs">how about you try your luck...</p>
             </article>
 
